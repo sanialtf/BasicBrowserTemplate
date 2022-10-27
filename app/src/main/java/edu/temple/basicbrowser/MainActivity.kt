05a19a993033417ddfc2c907340c0914f9feae6a
Sania <sanial369@gmail.com>
2022-10-27 12:19:20 -0400

    done

--- app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
+++ app/src/main/java/edu/temple/basicbrowser/MainActivity.kt
@@ -2,16 +2,31 @@ package edu.temple.basicbrowser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.android.volley.toolbox.StringRequest
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var urlEditText: EditText
-    lateinit var goButton: Button
+    lateinit var goButton: ImageButton
     lateinit var webView: WebView
+    lateinit var url: String
+    lateinit var requestQueue: RequestQueue
+
+//    val downloadHandler = Handler(Looper.getMainLooper()) {
+//
+//        true
+//    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +36,41 @@ class MainActivity : AppCompatActivity() {
         goButton = findViewById(R.id.goButton)
         webView = findViewById(R.id.webView)
 
+        requestQueue = Volley.newRequestQueue(this)
+
+        //Enable JavaScript
+        webView.settings.javaScriptEnabled = true
+
+        //Userin
+        goButton.setOnClickListener{
+            url = urlEditText.text.toString()
+
+
+        }
+
         // Allow your browser to intercept hyperlink clicks
-        webView.webViewClient = WebViewClient()
+        webView.webViewClient = object: WebViewClient(){
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                StringRequest(Request.Method.GET, url, {
+                    webView.loadDataWithBaseURL("", it, "text/html", "utf-8", null)
+                }, {})
+
+            }
+
+        }
+//        requestQueue.add(
+//            StringRequest(Request.Method.GET, "https://www.temple.edu", {
+//                webView.loadDataWithBaseURL("", it, "text/html", "utf-8", null)
+//            }, {})
+//        )
+
+
+
+
+
+
+
 
     }
 }
